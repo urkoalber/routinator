fe9ae664101e3342fbed05f64f7a73b6aa4791ec
urkoalber <urkito91@hotmail.com>
2013-12-14 23:00:35 +0100

    Hector me odia porque he cambiado su funcion

--- routinator.rb
+++ routinator.rb
@@ -6,6 +6,21 @@ require 'rubygems'
 class User
   attr_accessor :age, :bodyfat, :constitution, :gender, :activity_level, :weight_target, :aerobic_target,
                 :anaerobic_target, :dedicated_days
+
+  def set_constitution
+    constitution = nil
+    constitution = :thin if gender == "hombre" and (((15..20) === age and bodyfat <= 12) or ((21..35) === age and bodyfat <= 13) or ((36..50) === age and bodyfat <= 15) or (age > 50 and bodyfat <= 16))
+    constitution = :healthy if gender == "hombre" and (((15..20) === age and (13..18) === bodyfat) or ((21..35) === age and (14..19) === bodyfat) or ((36..50) === age and (16..21) === bodyfat) or (age > 50 and (17..22) === bodyfat))
+    constitution = :overweight if gender == "hombre" and (((15..20) === age and (19..24) === bodyfat) or ((21..35) === age and (20..25) === bodyfat) or ((36..50) === age and (22..27) === bodyfat) or (age > 50 and (23..28) === bodyfat))
+    constitution = :obese if gender == "hombre" and (((15..20) === age and bodyfat >= 25) or ((21..35) === age and bodyfat >= 26) or ((36..50) === age and bodyfat >= 28) or (age > 50 and bodyfat >= 29))
+
+    constitution = :thin if gender == "mujer" and (((15..20) === age and bodyfat <= 16) or ((21..35) === age and bodyfat <= 17) or ((36..50) === age and bodyfat <= 19) or (age > 50 and bodyfat <= 20))
+    constitution = :healthy if gender == "mujer" and (((15..20) === age and (17..22) === bodyfat) or ((21..35) === age and (18..23) === bodyfat) or ((36..50) === age and (20..25) === bodyfat) or (age > 50 and (21..26) === bodyfat))
+    constitution = :overweight if gender == "mujer" and (((15..20) === age and (23..28) === bodyfat) or ((21..35) === age and (24..29) === bodyfat) or ((36..50) === age and (26..31) === bodyfat) or (age > 50 and (27..32) === bodyfat))
+    constitution = :obese if gender == "mujer" and (((15..20) === age and bodyfat >= 29) or ((21..35) === age and bodyfat >= 30) or ((36..50) === age and bodyfat >= 32) or (age > 50 and bodyfat >= 33))
+
+    self.constitution = constitution
+  end
 end
 
 class Problem
@@ -33,58 +48,23 @@ class Routine
   attr_accessor :type, :muscle_days, :cardio_days
 end
 
-def check_constitution(gender, age, bodyfat)
-	if gender == "hombre" and age >= 15 and age <= 20 and bodyfat <= 12 then #R1
-		constitution = "delgado"
-		return constitution
-		elsif gender == "hombre" and age >= 15 and age <= 20 and bodyfat >= 13 and bodyfat <= 18 then #R2
-			constitution = "saludable"
-			return constitution
-			elsif gender == "hombre" and age >= 15 and age <= 20 and bodyfat >= 19 and bodyfat <= 24 then #R3
-				constitution = "sobrepeso"
-				return constitution
-				elsif gender == "hombre" and age >= 15 and age <= 20 and bodyfat >= 25 then #R4
-					constitution = "obeso"
-					return constitution
-					elsif gender == "hombre" and age >= 20 and age <= 35 and bodyfat <= 13 then  #R5
-						constitution = "delgado"
-						return constitution
-						elsif gender == "hombre" and age >= 20 and age <=35 and bodyfat >= 14 and bodyfat <=19 then  #R6
-							constitution = "saludable"
-							return constitution
-							elsif gender == "hombre" and age >= 20 and age <=35 and bodyfat >= 20 and bodyfat <=25 then #R7
-								constitution = "sobrepeso"
-								return constitution
-								elsif gender == "hombre" and age >= 20 and age <=35 and bodyfat >= 26 then #
-									constitution = "obeso"
-									return constitution
-									elsif gender == "hombre" and age >= 35 and age <=50 and bodyfat <=15 then #R9
-										constitution = "delgado"
-										return constitution
-										elsif gender == "hombre" and age >= 35 and age <=50 and bodyfat >= 16 and bodyfat <=21 then #R10
-											constitution = "saludable"
-											return constitution
-	end	
+user = User.new
+puts "Introduce tu edad:"
+user.age = gets.chomp.to_i
+puts "Introduce tu bodyfat (sin porcentaje):"
+user.bodyfat = gets.chomp.to_i
+puts "Introduce tu sexo:"
+user.gender = gets.chomp
+puts "Indica tu nivel de actividad (activo, medio, sedentario):"
+user.activity_level = gets.chomp
+puts "Indica tu objetivo de peso (ganar, mantener, perder):"
+user.weight_target = gets.chomp
+puts "Indica tu objetivo anaeróbico (fuerza, hipertrofia, resistencia):"
+user.anaerobic_target = gets.chomp
+unless user.weight_target == "ganar"
+	puts "Indica tu objetivo aeróbico (explosividad, media-distancia, larga-distancia):"
+  user.aerobic_target = gets.chomp
 end
-
-
-puts "Introduce tu edad"
-age = gets.chomp.to_i
-puts "Introduce tu bodyfat (sin porcentaje)"
-bodyfat = gets.chomp.to_i
-puts "Introduce tu sexo"
-gender = gets.chomp
-puts "Indica tu nivel de actividad (activo, medio, sedentario)"
-activity_level = gets.chomp
-puts "Indica tu objetivo de peso (ganar, mantener, perder)"
-weight_target = gets.chomp
-puts "Indica tu objetivo anaeróbico (fuerza, hipertrofia, resistencia)"
-anaerobic_target = gets.chomp
-if weight_target != "ganar" then
-	puts "Indica tu objetivo aeróbico (explosividad, media-distancia, larga-distancia)"
-	aerobic_target = gets.chomp
-end
-puts "Indica el numero de días que quieres dedicarle a la semana (2-6)"
-dedicated_days = gets.chomp.to_i
-
-constitution = check_constitution(gender, age, bodyfat)
+puts "Indica el numero de días que quieres dedicarle a la semana (2-6):"
+user.dedicated_days = gets.chomp.to_i
+user.set_constitution
